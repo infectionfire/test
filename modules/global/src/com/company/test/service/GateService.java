8ecc12492586da0bfeec5a7da856d5153c30a574
infectionfire <infectionfire0911@gmail.com>
2023-10-09 21:01:17 +0300

    док

--- modules/global/src/com/company/test/service/GateService.java
+++ modules/global/src/com/company/test/service/GateService.java
@@ -1,6 +1,5 @@
 package com.company.test.service;
 
-import com.company.test.entity.Gate;
 import com.company.test.entity.Planin;
 
 import java.util.List;
@@ -8,7 +7,17 @@ import java.util.List;
 public interface GateService {
     String NAME = "test_GateService";
 
+    /**
+     * Метод для получения списка всех доступных ворот в данный временной период
+     * @return список номеров для установки в сущность планирования
+     */
     List<String> findAvailableGateNumbers();
 
+
+    /**
+     * Установка статусов назначения ворот в приемку после выбора ворот
+     * @param planin - Приемка
+     * @param gateNumber - номер ворот
+     */
     void postRegistredGate(Planin planin, String gateNumber);
 }
--- modules/global/src/com/company/test/service/PlaninService.java
+++ modules/global/src/com/company/test/service/PlaninService.java
@@ -9,9 +9,26 @@ public interface PlaninService {
     String NAME = "test_PlaninService";
 
 
+    /**
+     * Метод для регистрации приемки
+     * @param carNumber - номер машины
+     * @param name - ФИО
+     * @param phoneNumber - номер телефона
+     * @param loadCapacity - загрузка
+     */
     void createNewPlaninByRegisterField(String carNumber, String name, String phoneNumber, LoadCapacity loadCapacity);
 
+
+    /**
+     * Метод для завершения загрузки. Ставит статус и состояние "Выезд разрешен"
+     * @param planin - сущность по которой велась приемка
+     */
     void postLoadUpdate(Planin planin);
 
+
+    /**
+     * Метод для поиска приемок, которым следует установить статус "покинул" по истечению времени
+     * @return Список приемок с истекшим периодом
+     */
     List<Planin> findCandidatesToLeft();
 }
